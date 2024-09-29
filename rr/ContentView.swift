0f51a57eae7101564f01e7712bec808dd9428350
Reema-M0 <Reemaal200200@gmil.com>
2024-09-29 00:31:13 -0700

    Initial Commit

---
+++ rr/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  rr
+//
+//  Created by Reem on 25/03/1446 AH.
+//
+
+import SwiftUI
+
+VS
+            
+    }
+}
+
+}
---
+++ rr/rrApp.swift
@@ -0,0 +1,17 @@
+//
+//  rrApp.swift
+//  rr
+//
+//  Created by Reem on 25/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct rrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
